a48c2cc55216668762808b7bc20ed1370a6c7a1d
FrancoisBosma <francois.bosma@hotmail.fr>
2026-08-09 17:59:40 +0200

    chore(db): add TODO comment in app code

--- app/composables/useTauriDb.ts
+++ app/composables/useTauriDb.ts
@@ -1,5 +1,6 @@
 import Database from "@tauri-apps/plugin-sql"
 export default async function useTauriDb() {
+  // TODO: move db data to process.env or smth
   const tauriDb = await Database.load("sqlite:userdata.db")
   return { tauriDb }
 }
